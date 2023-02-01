3a31f1dc1af883ea4030bf7a6ef20c82b0c75a92
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2023-02-01 22:25:33 +0300

    tt

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def members(request):
+  template = loader.get_template('myfirst.html')
+  return HttpResponse(template.render())
