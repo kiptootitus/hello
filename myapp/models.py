426aad8461607622b1a38f6a5d55f27109d11686
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2023-02-02 23:25:08 +0300

    Updates

--- myapp/models.py
+++ myapp/models.py
@@ -6,4 +6,6 @@ class myapp(models.Model):
   phone = models.IntegerField(null=True)
   joined_date = models.DateField(null=True)
   
-  
+# myapp2=myapp(firstname='Tobias', lastname='Refsnes', phone='0705839228',joined_date='2023-03-04')
+# myapp3=myapp(firstname='Meshack', lastname='Kibiwott', phone='0100436617',joined_date='2022-11-04')    
+# myapp4=myapp(firstname='Brian', lastname='Kibet', phone='0700436617',joined_date='2020-11-04')    
