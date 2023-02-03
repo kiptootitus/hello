38e9f6a6d8ef9b5e5e3e97673588c31e72292287
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2023-02-03 16:00:57 +0300

    display of contents

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import myapp
 
 def members(request):
-  template = loader.get_template('myfirst.html')
-  return HttpResponse(template.render())
+  myapps = myapp.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'myapp': myapps,
+  }
+  return HttpResponse(template.render(context, request))
