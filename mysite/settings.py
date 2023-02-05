06b6a35cd0220aa3dee4dff76dd0b932fa7d07c9
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2023-02-05 10:59:23 +0300

    404 file update

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)35qin3l#icp(%qlv9jm3d^w96ye&w(b2g+d)d1n=@!+=gtkyi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
